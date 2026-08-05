1361cee3e8b4868dd27c63dc1b371a648492951f
磯陽向人 <40888437+hinato150@users.noreply.github.com>
2026-08-05 15:25:03 +0900

    退出しましたがメッセージ順に表示される

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -64,7 +64,7 @@ const onReceiveEnter = (data) => {
 
 // サーバから受信した退室メッセージを受け取り画面上に表示する
 const onReceiveExit = (data) => {
-      chatList.push(`${data.userName} さんが退室しました`)
+      chatList.unshift(`${data.userName} さんが退室しました`)
 }
 
 // サーバから受信した投稿メッセージを画面上に表示する
